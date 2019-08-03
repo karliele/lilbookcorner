102ff76de196f402163087fa12344b3206f4361e
karliele <48388748+karliele@users.noreply.github.com>
2019-08-04 00:33:34 +0700

    add information

---
+++ js/tipuesearch_content.js
@@ -0,0 +1,4 @@
+var tipuesearch = {"pages": [
+     {"title": "Tipue", "text": "Tipue is a small web development studio based in North London.", "tags": "jQuery HTML5 CSS", "url": "http://www.tipue.com"},
+     {"title": "Tipue Search, a site search engine jQuery plugin", "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source, responsive and fast. Tipue Search only needs a browser that supports jQuery. It doesn't need MySQL or similar. In Static mode it doesn't even need a web server.", "tags": "JavaScript", "url": "http://www.tipue.com/search"}
+]};
